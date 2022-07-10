d993e4f843f416bad7d39b01de8f6a804f5137bc
El-Litios <carlosguzmanrojas00@gmail.com>
2022-07-10 18:35:47 -0400

    Logica de buscador

--- GIMNASIO/GIMNASIO/Controllers/HomeController.cs
+++ GIMNASIO/GIMNASIO/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using GIMNASIO.Models;
+﻿
+using GIMNASIO.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -24,6 +26,7 @@ namespace GIMNASIO.Controllers
 
         public IActionResult Index(int page)
         {
+            ViewData["CategoriaId"] = new SelectList(_context.tblCategorias.ToList(), "CategoriaId", "Nombre");
             IndexViewModel indexViewModel = new IndexViewModel();
 
             if (page == 0)
@@ -72,6 +75,42 @@ namespace GIMNASIO.Controllers
             return View(_context.tblProductos.Where(p => p.ProductoId.Equals(ProductoId)).
                 Include(e => e.Categoria)
                 .Include(e => e.Estado).ToList());
+        }
+
+
+        public IActionResult Search(int page, Categoria C)
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.tblCategorias.ToList(), "CategoriaId", "Nombre");
+            IndexViewModel indexViewModel = new IndexViewModel();
+
+            if (page == 0)
+            {
+                indexViewModel.Pagina = 1;
+            }
+            else
+            {
+                indexViewModel.Pagina = page;
+            }
+
+            int muestra = 6;
+            int cantidad = _context.tblProductos.ToList().Count / muestra;
+            if (cantidad % muestra == 0)
+            {
+                indexViewModel.CantidadPagina = cantidad;
+            }
+            else
+            {
+                indexViewModel.CantidadPagina = cantidad + 1;
+            }
+
+            indexViewModel.ListaProductos = _context.tblProductos.Include(e => e.Categoria)
+                .Include(e => e.Estado)
+                .Where(p => p.CategoriaId == C.CategoriaId)
+                .Skip((indexViewModel.Pagina - 1) * muestra)
+                .Take(6).ToList();
+            TempData["NextPage"] = indexViewModel.Pagina + 1;
+            TempData["PreviusPage"] = indexViewModel.Pagina - 1;
+            return View(indexViewModel);
         }
 
     }
