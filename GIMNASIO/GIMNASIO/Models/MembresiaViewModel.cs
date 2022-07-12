1f963d0041f177c089f23f272f35f0caf91a142d
El-Litios <carlosguzmanrojas00@gmail.com>
2022-07-11 23:14:11 -0400

    viewmodel de membresia

---
+++ GIMNASIO/GIMNASIO/Models/MembresiaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIMNASIO.Models
+{
+    public class MembresiaViewModel
+    {
+        public List<Membresia> ListaMembresias { get; set; }
+
+        [Display(Name = "Metodo de Pago")]
+        public int MetodoPagoMembresiaId { get; set; }
+        public MetodoPagoMembresia MetodoPagoMembresia { get; set; }
+
+        [Display(Name = "Estado de Membresia")]
+        public int EstadoMembresiaId { get; set; }
+        public EstadoMembresia EstadoMembresia { get; set; }
+
+        [Display(Name = "Tipo de Membresia")]
+        public int TipoMembresiaId { get; set; }
+        public TipoMembresia TipoMembresia { get; set; }
+
+    }
+}
