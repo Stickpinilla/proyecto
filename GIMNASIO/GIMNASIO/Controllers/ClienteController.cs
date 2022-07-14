8d196b61075376aa03006011af62efbece58486d
Jonathan <jonathan.pinilla@inacapmail.cl>
2022-07-14 10:01:14 -0400

    Cambios de diseño

--- GIMNASIO/GIMNASIO/Controllers/ClienteController.cs
+++ GIMNASIO/GIMNASIO/Controllers/ClienteController.cs
@@ -29,7 +29,7 @@ namespace GIMNASIO.Controllers
 
         public IActionResult ListarCliente()
         {
-            var clientelista = _context.Users.ToList();
+            var clientelista = _context.Users.Where(t => t.Tipo == "Cliente").ToList();
             return View(clientelista);
         }
 
@@ -93,6 +93,101 @@ namespace GIMNASIO.Controllers
                 .Include(c => c.Categoria)
                 .Include(e => e.Estado)
                 .ToList());
+        }
+
+
+
+        public IActionResult ListarEntrenadores()
+        {
+            var entrenadorlista = _context.Users.Where(t => t.Tipo == "Entrenador").ToList();
+            return View(entrenadorlista);
+        }
+
+
+        //llamar al registro entrenador
+        [HttpGet]
+        public IActionResult RegistroEntrenador()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegistroEntrenador(RegistroViewModel Rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var User = new Cliente()
+                {
+                    Rut = Rvm.Rut,
+                    Nombres = Rvm.Nombres,
+                    ApellidoP = Rvm.ApellidoP,
+                    ApellidoM = Rvm.ApellidoM,
+                    Correo = Rvm.Correo,
+                    Email = Rvm.Correo,
+                    Telefono = Rvm.Telefono,
+                    Direccion = Rvm.Direccion,
+                    Ciudad = Rvm.Ciudad,
+                    fecha = Rvm.fecha.Date,
+                    UserName = Rvm.Correo,
+                    Tipo = "Entrenador"
+                };
+                var resultado = await _userManager.CreateAsync(User, Rvm.Pass);
+                if (resultado.Succeeded)
+                {
+                    //TODOS LOS USUARIOS REGISTRADOS SE INGRESEN CON ESE CLAIM
+                    await _userManager.AddClaimAsync(User, new System.Security.Claims.Claim("Entrenador", "10"));
+                    return RedirectToAction(nameof(ListarEntrenadores));
+                }
+
+            }
+            return View();
+        }
+
+
+        public IActionResult ListarAdmin()
+        {
+            var adminlista = _context.Users.Where(t => t.Tipo == "Admin").ToList();
+            return View(adminlista);
+        }
+
+
+        //llamar al registro admin
+        [HttpGet]
+        public IActionResult RegistroAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegistroAdmin(RegistroViewModel Rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var User = new Cliente()
+                {
+                    Rut = Rvm.Rut,
+                    Nombres = Rvm.Nombres,
+                    ApellidoP = Rvm.ApellidoP,
+                    ApellidoM = Rvm.ApellidoM,
+                    Correo = Rvm.Correo,
+                    Email = Rvm.Correo,
+                    Telefono = Rvm.Telefono,
+                    Direccion = Rvm.Direccion,
+                    Ciudad = Rvm.Ciudad,
+                    fecha = Rvm.fecha.Date,
+                    UserName = Rvm.Correo,
+                    Tipo = "Admin"
+                };
+                var resultado = await _userManager.CreateAsync(User, Rvm.Pass);
+                if (resultado.Succeeded)
+                {
+                    //TODOS LOS USUARIOS REGISTRADOS SE INGRESEN CON ESE CLAIM
+                    await _userManager.AddClaimAsync(User, new System.Security.Claims.Claim("Admin", "10"));
+                    return RedirectToAction(nameof(ListarAdmin));
+                }
+
+            }
+            return View();
         }
 
     }
