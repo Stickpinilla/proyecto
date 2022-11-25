09c694e0da6ab5258996abf8598dd76dbd27b7a0
El-litios <carlosguzmanrojas00@gmail.com>
2022-11-25 13:42:57 -0300

    Controlador de maquinarias

---
+++ GIMNASIO/GIMNASIO/Controllers/MaquinariaController.cs
@@ -0,0 +1,23 @@
+﻿using GIMNASIO.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GIMNASIO.Controllers
+{
+    [Authorize(Policy = "PolicyAdmin")]
+    public class MaquinariaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MaquinariaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ListaMaquinaria()
+        {
+            return View();
+        }
+    }
+}
