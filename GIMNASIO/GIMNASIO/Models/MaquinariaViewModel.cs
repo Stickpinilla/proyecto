6bdca45be84359109f85a54ba146dd4c127b3e9c
El-litios <carlosguzmanrojas00@gmail.com>
2022-11-29 18:04:24 -0300

    ViewModel de maquinaria para paginacion

---
+++ GIMNASIO/GIMNASIO/Models/MaquinariaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace GIMNASIO.Models
+{
+    public class MaquinariaViewModel
+    {
+        public List<Maquinaria> ListaMaquinaria { get; set; }
+
+        public int CantidadPagina { get; set; }
+
+        public int Pagina { get; set; }
+
+        [Display(Name = "EstadoMaquinaria")]
+        public int EstadoId { get; set; }
+        public EstadoMaquinaria EstadoMaquinaria { get; set; }
+
+        [Display(Name = "TipoMaquinaria")]
+        public int TipoId { get; set; }
+        public TipoMaquinaria TipoMaquinaria { get; set; }
+
+    }
+}
