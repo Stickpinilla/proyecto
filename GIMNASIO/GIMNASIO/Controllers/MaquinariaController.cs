ff88294d558f54ae808032af92a077de948fc3aa
El-litios <carlosguzmanrojas00@gmail.com>
2022-12-01 15:25:02 -0300

    Funciones de agregar nueva maquinaria e inhabilitar maquinaria

--- GIMNASIO/GIMNASIO/Controllers/MaquinariaController.cs
+++ GIMNASIO/GIMNASIO/Controllers/MaquinariaController.cs
@@ -4,7 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace GIMNASIO.Controllers
 {
@@ -19,8 +23,13 @@ namespace GIMNASIO.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListaMaquinaria(int page)
+        public IActionResult ListaMaquinaria(int page, int EstadoId)
         {
+            int estado = 1;
+            if (EstadoId != estado) {
+                estado = EstadoId;
+            }
+
             ViewData["TipoId"] = new SelectList(_context.tblTipoMaquinaria.ToList(), "TipoMaquinariaId", "TipoMaquinariaNombre");
             ViewData["EstadoId"] = new SelectList(_context.tblEstadoMaquinaria.ToList(), "EstadoMaquinariaId", "EstadoMaquinariaNombre");
             MaquinariaViewModel viewModel = new MaquinariaViewModel();
@@ -44,13 +53,50 @@ namespace GIMNASIO.Controllers
                 viewModel.CantidadPagina = cantidad + 1;
             }
 
-            viewModel.ListaMaquinaria = _context.tblMaquinaria.Include(m => m.TipoMaquinaria)
+            viewModel.ListaMaquinaria = _context.tblMaquinaria
+                .Where(m => m.EstadoMaquinariaId == estado)
+                .Include(m => m.TipoMaquinaria)
                 .Include(m => m.EstadoMaquinaria)
                 .Skip((viewModel.Pagina - 1) * muestra)
                 .Take(6).ToList();
             TempData["NextPage"] = viewModel.Pagina + 1;
             TempData["PreviusPage"] = viewModel.Pagina - 1;
+            TempData["Estado"] = estado;
+            //TempData["Mensaje"] = "Maquinaria Ingresada Exitosamente!";
             return View(viewModel);
         }
+
+        [HttpGet]
+        public IActionResult CrearMaquinaria()
+        {
+            ViewData["EstadoMaquinariaId"] = new SelectList(_context.tblEstadoMaquinaria.ToList(), "EstadoMaquinariaId", "EstadoMaquinariaNombre");
+            ViewData["TipoMaquinariaId"] = new SelectList(_context.tblTipoMaquinaria.ToList(), "TipoMaquinariaId", "TipoMaquinariaNombre");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearMaquinaria(Maquinaria M)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(M);
+                await _context.SaveChangesAsync();
+                TempData["Mensaje"] = "Maquinaria Agregado Exitosamente!";
+                return RedirectToAction("ListaMaquinaria", new { EstadoId = 1 });
+            }
+            return View(M);
+        }
+
+        //Inhabilitar maquinaria por Id
+        [HttpGet]
+        public async Task<IActionResult> InhabilitarMaquinaria(int MaquinariaId)
+        {
+            var MaquinariaInhabilitada = _context.tblMaquinaria.Where(m => m.MaquinariaId == MaquinariaId).FirstOrDefault();
+            MaquinariaInhabilitada.EstadoMaquinariaId = 2;//estado: inhabilitado
+            await _context.SaveChangesAsync();
+            return RedirectToAction("ListaMaquinaria", new { EstadoId = 1 });
+        }
+
+
     }
 }
