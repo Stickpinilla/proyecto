65f701c31902b94c5f4ad1c33d4a54d6cd3ae6be
El-litios <carlosguzmanrojas00@gmail.com>
2022-12-02 14:59:28 -0300

    View Model de Mantencion y Maquinaria

---
+++ GIMNASIO/GIMNASIO/Controllers/MantencionController.cs
@@ -0,0 +1,53 @@
+﻿using GIMNASIO.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GIMNASIO.Controllers
+{
+    [Authorize(Policy = "PolicyAdmin")]
+    public class MantencionController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MantencionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult AgregarMantencionPorMaquina(int MaquinaId)
+        {
+            MaquinariaMantencionViewModel mmvm = new MaquinariaMantencionViewModel();
+            mmvm.Maquinaria = _context.tblMaquinaria
+                .Where(m => m.MaquinariaId == MaquinaId)
+                .Include(m => m.TipoMaquinaria)
+                .Include(m => m.EstadoMaquinaria)
+                .FirstOrDefault();
+
+            mmvm.MaquinariaId = MaquinaId;
+            //mmvm.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            ViewData["EstadoMantencionId"] = new SelectList(_context.tblEstadoMantencion.ToList(), "EstadoMantencionId", "EstadoMantencionNombre");
+
+            return View(mmvm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarMantencionPorMaquina(Mantencion M)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(M);
+                await _context.SaveChangesAsync();
+                TempData["Mensaje"] = "Maquinaria Agregado Exitosamente!";
+                return RedirectToAction("ListaMaquinaria", new { EstadoId = 1 });
+            }
+            return View(M);
+        }
+    }
+}
---
+++ GIMNASIO/GIMNASIO/Models/MaquinariaMantencionViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace GIMNASIO.Models
+{
+    public class MaquinariaMantencionViewModel
+    {
+        public Maquinaria Maquinaria { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime MantencionFechaIngreso { get; set; }
+
+        public string MantencionDescripcion { get; set; }
+
+        public int EstadoMantencionId { get; set; }
+
+        public int MaquinariaId { get; set; }
+
+        public int UserId { get; set; }
+
+    }
+}
+
