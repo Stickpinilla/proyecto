1bf65e883d7a1af7adf4dfbfec9f2d0d01da2f1e
El-Litios <carlosguzmanrojas00@gmail.com>
2022-12-23 01:11:32 -0300

    Migraciones

---
+++ GIMNASIO/GIMNASIO/Migrations/20221223035056_MIGRATION3.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GIMNASIO.Migrations
+{
+    public partial class MIGRATION3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tblMembresia_AspNetUsers_ClienteId",
+                table: "tblMembresia");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tblMembresia_AspNetUsers_ClienteId",
+                table: "tblMembresia",
+                column: "ClienteId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tblMembresia_AspNetUsers_ClienteId",
+                table: "tblMembresia");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tblMembresia_AspNetUsers_ClienteId",
+                table: "tblMembresia",
+                column: "ClienteId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
