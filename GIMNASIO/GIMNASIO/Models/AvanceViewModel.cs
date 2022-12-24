bc1963f993ca18bb67e92a83cc01d78e2dc0aef8
El-litios <carlosguzmanrojas00@gmail.com>
2022-12-24 18:59:38 -0300

    Avance View Model

---
+++ GIMNASIO/GIMNASIO/Models/AvanceViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GIMNASIO.Models
+{
+    public class AvanceViewModel
+    {
+        public List<AvanceCliente> ListaAvance { get; set; }
+        public Double AvanceCliente_Peso { get; set; }
+
+        public Double AvanceCliente_Altura { get; set; }
+
+        public float AvanceCliente_IMC { get; set; }
+        
+        public DateTime AvanceCliente_Fecha { get; set; }
+        public string AvanceCliente_Situacion { get; set; }
+        public string Id { get; set; }
+
+    }
+}
